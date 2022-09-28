3831416f2c6cef4b87643b9b7d976d282b35629a
Andrey Markov <arherower@outlook.com>
2022-09-28 09:38:26 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,50 @@
+﻿namespace Задание_4._4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n;
+            int m;
+            Random rnd = new Random();
+            //while (Console.ReadKey! = ConsoleKey.Escape)
+            while(true)
+            {
+                Console.WriteLine("Существует камера хранения с количеством ячеек N. ");
+                Console.Write("Введите N: ");
+                n = int.Parse(Console.ReadLine());
+                int[] cellArray = new int[n];
+                for (int x = 0; x < cellArray.Length; x++)
+                {
+                    cellArray[x] = rnd.Next(0, 11);
+                    Console.Write(cellArray[x] + " ");
+                }
+                Console.WriteLine("\nПо ячейкам распределены вещи массой до 10 кг. Возможны пустые ячейки.");
+                Console.WriteLine("Общая масса: " + cellArray.Sum() + "\n");
+                Console.Write("Нужно изъять вещи из каждой M кратной ячейки.\nВведите М: ");
+                m = int.Parse(Console.ReadLine());
+                int extract = 0;
+                //foreach (int value in cellArray)
+                //{
+                //    if (value % m == 0)
+                //    {
+                //        int x = value;
+                //        cellArray[x] = 0;
+                //    }
+                //}
+                for (int x = 1; x < cellArray.Length; x++)
+                {
+                    
+                    if (x %(m-1) == 0)
+                     {
+                        cellArray[x] = 0;
+                    }
+                }
+                Console.WriteLine("Оставшаяся масса: " + cellArray.Sum());
+                Console.WriteLine();
+            }
+            Console.WriteLine("End");
+            Console.ReadKey();
+        }
+    }
+}
